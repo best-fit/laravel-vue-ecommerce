80e691dae88f27836ee70c68e79110c7a4e0febf
renelimadev <renelima.dev@gmail.com>
2017-02-04 16:47:38 -0800

    AppServiceProvider.php added

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\ShoppingCart;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+      View::composer('*', function($view){
+        // checking if there's a SC attached on the users session
+        $sessionName = 'shopping_cart_id';
+        $scid = \Session::get($sessionName);
+        $shopping_cart = ShoppingCart::findOrCreateById($scid);
+        \Session::put($sessionName, $shopping_cart->id);
+        // send the data to views
+        $view->with('productsCount', $shopping_cart->productsCount());
+      });
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
