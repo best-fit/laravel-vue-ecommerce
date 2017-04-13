7886a2298cd454786f52fc2529c98725eb031f63
renelimadev <renelima.dev@gmail.com>
2017-04-13 15:39:26 -0700

    SetShoppingCart.php added

---
+++ app/Http/Middleware/SetShoppingCart.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\ShoppingCart;
+
+class SetShoppingCart
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // detecting existing SC
+        $sessionName = 'shopping_cart_id';
+        $scid = \Session::get($sessionName);
+        $shopping_cart = ShoppingCart::findOrCreateById($scid);
+        \Session::put($sessionName, $shopping_cart->id);
+        // send it to controller
+        $request->shopping_cart = $shopping_cart;
+        return $next($request);
+    }
+}
