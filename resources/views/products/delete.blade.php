98bbe3db35cfb918195e8026d9f80c693cbe27b7
renelimadev <renelima.dev@gmail.com>
2017-04-29 09:21:20 -0700

    delete.blade.php updated

--- resources/views/products/delete.blade.php
+++
@@ -1,7 +0,0 @@
-@auth
-
-{!! Form::open(['method' => 'DELETE', 'route' => ['products.destroy', $product->id], 'onsubmit' => 'return confirm("Are you sure about deleting this product?")', 'class' => 'text-center' ]) !!}
-  <button type="submit" class="btn btn-link text-danger delete-button">Delete product</button>
-{!! Form::close() !!}
-
-@endauth
